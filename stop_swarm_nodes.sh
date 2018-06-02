7af8818cd08f49cc728370e32e02b60fac25f25a
Harsha Vardhan Jagadeesan <vardhanharshaj@gmail.com>
2018-06-02 11:40:54 +0530

    Ability to accept arguments with expanded scope
    Have added the ability to accept three command line arguments to the
    script. Currently, it accepts 'start', 'stop', and 'help'. The 'start'
    argument, as the name states, starts all the machines that are in a
    stopped state using 'docker-machine'. The 'stop' argument stops all the
    machines that are in a running state using the 'docker-machine' command.
    The 'help' argument displays a usage message that details how the script
    can be used and what arguments it accepts. '-h', '--help', 'help', and
    any other argument that doesn't match any of the aforementioned
    arguments will display the same usage message.

--- stop_swarm_nodes.sh
+++ stop_swarm_nodes.sh
@@ -3,14 +3,36 @@
 #                                                          # 
 #             Maintainer: Harsha Vardhan J                 #
 #                                                          #
-#    bash script for stopping all machines started by      #
-#              'docker-machine' command.                   #
+#    bash script for starting and stopping all machines    #
+#      		created by 'docker-machine' command.           #
 #                                                          #
 ############################################################
 
-ARRAY=($(docker-machine ls | grep -v "NAME" | grep -v "machine does not exist" | grep "Running" | awk '{print $1}'))
+ARG="$1"
 
-for MACHINE in "${ARRAY[@]}"
-do
-	docker-machine stop "${MACHINE}"
-done
+case "${ARG}" in
+	start)
+		ARRAY=($(docker-machine ls | grep -v "NAME" | grep -v "machine does not exist" | grep "Running" | awk '{print $1}'))
+		printf "\nStarting nodes...\n"
+		for MACHINE in "${ARRAY[@]}"
+		do
+			docker-machine stop "${MACHINE}"
+		done
+		;;
+	stop)
+		ARRAY=($(docker-machine ls | grep -v "NAME" | grep -v "machine does not exist" | grep "Running" | awk '{print $1}'))
+		printf "\nStopping nodes...\n"
+		for MACHINE in "${ARRAY[@]}"
+		do
+			docker-machine start "${MACHINE}"
+		done
+		;;
+	--help|-h|help|?)
+		printf "\nThis script accepts either 'start' or 'stop' as arguments on which it performs actions.\n\
+			\n\t'start' --->\tThe script proceeds to start all the machines listed by 'docker-machine ls' \
+			\n\t\t\tthat are in stopped state.\n\
+			\n\t'stop'\t--->\tThe script proceeds to stop all the machines listend by 'docker-machine ls' \
+			\n\t\t\tthat are in running state.\n\
+			\n\t'help'\t--->\tThe script displays this usage message.\n"
+		;;
+esac
