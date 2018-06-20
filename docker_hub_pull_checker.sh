eea30d1d8da2fd10d51f7602dc66bf508da58b19
Harsha Vardhan Jagadeesan <vardhanharshaj@gmail.com>
2018-06-20 21:59:46 +0530

    Added scripts to check pulls on Docker Hub
    Have added two scripts that check the number of pulls for a Docker image
    whose name is returned by a search certain search result.
    
    One of the scripts, 'docker_hub_pull_checker.sh' checks to see the
    number of pulls on the repository returned by the search term
    'harshavardhanj'.
    
    The other script, 'docker_hub_pull_checker.sh' checks to see the number
    of pulls on the repository returned by the search term that is given as
    an argument to the script.

---
+++ docker_hub_pull_checker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## Author       :   Harsha Vardhan J
+## License      :   MIT
+##
+## Description  :  This script downloads the link given below and check to see
+##                 how many pulls have been made against that image returned by the search in that
+##                 link.
+
+# Function to delete created temporary file(s)
+function finish
+{
+    for FILE in "${TEMP_FILE}"
+    do
+        if [ -e "${TEMP_FILE}" ]
+        then
+            cd $( dirname "${TEMP_FILE}" ) && rm -f $( basename "${FILE}" )
+        fi
+    done
+}
+
+# Trapping script exit(0), SIGHUP(1), SIGINT(2), SIGQUIT(3), SIGTRAP(5), SIGTERM(15) signals to cleanup temporary files
+trap finish 0 1 2 3 5 15
+
+# Creating temporary file to store downloaded webpage
+TEMP_FILE=$( mktemp -p "${TMPDIR}" wget.XXX )
+
+# Link to webpage that is to be downloaded
+LINK="https://hub.docker.com/search/?isAutomated=0&isOfficial=0&page=1&pullCount=0&q=harshavardhanj&starCount=0"
+
+# Downloading webpage and storing in temporary file
+wget "${LINK}" -O "${TEMP_FILE}" 2>/dev/null
+
+# Filtering required information i.e. "pull_count" from the downloaded webpage
+PULLS="$(cat "${TEMP_FILE}" | awk '/\,\"pull_count\":[0-9]*.\,/' | awk -F',"pull_count":' '{print $2}' | awk -F',' '{print $1}')"
+
+# Displaying number of pulls
+printf "\nNumber of pulls = "${PULLS}"\n"
+
+######################################### End of script ###########################################
---
+++ docker_hub_pull_checker_with_arg.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+## Author       :   Harsha Vardhan J
+## License      :   MIT
+##
+## Description  : 	This script downloads the link given by the search term and
+##                  check to see how many pulls have been made against that image returned by the
+##                  search in that link.
+
+
+# Function to delete created temporary file(s)
+function finish
+{
+    for FILE in "${TEMP_FILE}"
+    do
+        if [ -e "${TEMP_FILE}" ]
+        then
+            cd $( dirname "${TEMP_FILE}" ) && rm -f $( basename "${FILE}" )
+        fi
+    done
+}
+
+# Trapping script exit(0), SIGHUP(1), SIGINT(2), SIGQUIT(3), SIGTRAP(5), SIGTERM(15) signals to cleanup temporary files
+trap finish 0 1 2 3 5 15
+
+
+# Checking if arguments are given
+if [ $# -ne 1 ]
+then
+	printf "$0 : URL not provided as argument. \n USAGE: $0 URL"
+	exit 1
+elif [ $# -eq 1 ]
+then
+	# Creating temporary file to store downloaded webpage
+	TEMP_FILE=$( mktemp -p "${TMPDIR}" wget.XXX )
+#
+	# Variable 'SEARCH' assigned to first argument
+	SEARCH=$1
+#
+	# Link to webpage that is to be downloaded
+	LINK="https://hub.docker.com/search/?isAutomated=0&isOfficial=0&page=1&pullCount=0&q=$SEARCH&starCount=0"
+#
+	# Downloading webpage and storing in temporary file
+	wget "${LINK}" -O "${TEMP_FILE}" 2>/dev/null
+#
+	# Filtering required information i.e. "pull_count" from the downloaded webpage
+	PULLS="$(cat "${TEMP_FILE}" | awk '/\,\"pull_count\":[0-9]*.\,/' | awk -F',"pull_count":' '{print $2}' | awk -F',' '{print $1}')"
+#
+	# Displaying number of pulls
+	printf "\nNumber of pulls = "${PULLS}"\n"
+fi
+
+
+######################################### End of script ###########################################
