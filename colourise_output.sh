6e6913e4eb7d8c42a5c61a78d30eb274ade168db
Harsha Vardhan Jagadeesan <vardhanharshaj@gmail.com>
2018-09-19 12:58:31 +0530

    Added case-senstive matching
    The first argument to the script, which is the colour in which the
    string(which is the second argument) is to be printed, would previously
    match only lowercase values for the colour such as "red" or "Red".
    
    Now, any variation of case matches the string i.e., "Red", "rEd", "RED",
    and 'reD' all match the colour 'red'.
    
    Have also changed the variables to local, read-only type.

--- colourise_output.sh
+++ colourise_output.sh
@@ -1,59 +1,70 @@
 #!/usr/bin/env bash
 #
-#: Title        :	colorise_output.sh
+#: Title        :	colourise_output.sh
 #: Date         :	18-Aug-2018
 #: Author       :	"Harsha Vardhan J" <vardhanharshaj@gmail.com>
-#: Version      :	1.0
+#: Version      :	1.0 (Stable)
 #: Description  :	This script contains a function which
-#                	helps colorise a string.
-#: Options      :	
+#                	helps colourise any string.
+#: Options      :	Requires two arguments. First one is the colour, second is
+#					the string to be printed.
 #: Usage		:	The colour of the text is to be passed as the first argument.
 #					The text to be printed is the second argument. Note that the
-#					text should be passed in quotation marks.
+#					text should be passed in quotation marks. The colour can be
+#                   case-insensitive too.
 #					Example:	./colourise_output.sh blue "Text in blue"
+#								./colourise_output.sh BlUe "Text in blue"
 ################
 
 function colourise() {
 	if tput setaf 1 &> /dev/null; then
 		tput sgr0; # reset colors
-		bold=$(tput bold);
-		reset=$(tput sgr0);
+		local -r BOLD=$(tput bold);
+		local -r RESET=$(tput sgr0);
 		# Solarized colors, taken from http://git.io/solarized-colors.
-		black=$(tput setaf 0);
-		blue=$(tput setaf 33);
-		cyan=$(tput setaf 37);
-		green=$(tput setaf 64);
-		orange=$(tput setaf 166);
-		purple=$(tput setaf 125);
-		red=$(tput setaf 124);
-		violet=$(tput setaf 61);
-		white=$(tput setaf 15);
-		yellow=$(tput setaf 136);
+		local -r BLACK=$(tput setaf 0);
+		local -r BLUE=$(tput setaf 33);
+		local -r CYAN=$(tput setaf 37);
+		local -r GREEN=$(tput setaf 64);
+		local -r ORANGE=$(tput setaf 166);
+		local -r PURPLE=$(tput setaf 125);
+		local -r RED=$(tput setaf 124);
+		local -r VIOLET=$(tput setaf 61);
+		local -r WHITE=$(tput setaf 15);
+		local -r YELLOW=$(tput setaf 136);
 	else
-		bold='';
-		reset=$'\e[0m'
-		blue=$'\e[1;34m'
-		cyan=$'\e[1;36m'
-		green=$'\e[1;32m'
-		orange=$'\e[1;33m'
-		purple=$'\e[1;35m'
-		red=$'\e[1;31m'
-		violet=$'\e[1;35m'
-		white=$'\e[1;37m'
-		yellow=$'\e[1;33m'
+		local -r BOLD='';
+		local -r RESET=$'\e[0m'
+		local -r BLUE=$'\e[1;34m'
+		local -r CYAN=$'\e[1;36m'
+		local -r GREEN=$'\e[1;32m'
+		local -r ORANGE=$'\e[1;33m'
+		local -r PURPLE=$'\e[1;35m'
+		local -r RED=$'\e[1;31m'
+		local -r VIOLET=$'\e[1;35m'
+		local -r WHITE=$'\e[1;37m'
+		local -r YELLOW=$'\e[1;33m'
 	fi
 
-	if [[ $# -eq 2 && $1 =~ ^([Bb]lue|[Cc]yan|[Gg]reen|[Oo]range|[Pp]urple|[Rr]ed|[Vv]iolet|[Ww]hite|[Yy]ellow)$ ]]
+	# Set case-insensitive matching in 'bash'
+	# This is to help match the given colour in a case-insensitive fashion
+	shopt -qs nocasematch
+
+	if [[ $# -eq 2 && $1 =~ ^(blue|cyan|green|orange|purple|red|violet|white|yellow)$ ]]
 	then
-		local COLOUR="${1}"
-		local STRING="${2}"
+		local -r COLOUR="${1^^}"
+		local -r STRING="${2}"
 
-		printf "%s\n" "${!COLOUR}${STRING}${end}"
+		printf '%s\n' "${!COLOUR}${STRING}${RESET}"
 	else
-		printf "%s\n" "Incorrect usage"
-		printf "%-17s\t%s\n" "Usage : " "$0 colour \"Text to be coloured\""
-		printf "%-17s\t%s\n" "Colours accepted:" "blue, cyan, green, orange, purple, red, violet, white, yellow"
+		printf '%s\n' "Incorrect usage"
+		printf '%-17s\t%s\n' "Usage : " "$0 colour \"Text to be coloured\""
+		printf '%-17s\t%s\n' "Colours accepted:" "blue, cyan, green, orange, purple, red, violet, white, yellow"
 	fi
+
+	# Unset case-insensitive matching in 'bash'
+	# Case-sensitive matching is the default behaviour in 'bash'
+	shopt -qu nocasematch
 }
 
 colourise "$@"
