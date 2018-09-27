83c3391ae8afa430dcefc834cf314ac11db871a2
Harsha Vardhan Jagadeesan <vardhanharshaj@gmail.com>
2018-09-27 13:45:13 +0530

    Major changes to 'RHCE_vm.sh' script
    The following major changes have been made to the script:
    
        1). Have changed 'start_vm' function to 'control_vm'
    
    	The 'start_vm' function has been modified to include turning off
    	the VM or turning on the VM, depending on the argument received.
    
        2). Have added a 'check_input' function
    
    	The 'check_input' function, as the name states, checks if one
    	argument is given to the script and if the argument matches
    	either the string "on" or "off". If it does, the content of the
    	argument is added to the variable FLAG. Else, the usage of the
    	script is printed to the screen.
    
        3). The script only runs when either "on" or "off" is provided as an
    	argument(without the quotes). If no arguments(or incorrect ones)
    	are provided, the usage of the script will be printed to the
    	screen.
    
        4). Print statements have been added at every stage of the script to
    	notify the user of the progress.
    
        5). Changed version number of the script to 2.0 (Stable) after
    	running the 'shellcheck' and 'checkbashisms' tool against the
    	script. No errors have been raised by both tools. Also, during
    	the usage of the script, no issues were found.

--- RHCE_vm.sh
+++ RHCE_vm.sh
@@ -3,29 +3,52 @@
 #: Title        :	RHCE_vm.sh
 #: Date         :	14-Sep-2018
 #: Author       :	"Harsha Vardhan J" <vardhanharshaj@gmail.com>
-#: Version      :	1.0 (Stable)
-#: Description  :	This script first starts the VM tagged as "RHCE",
-#              		then it pings the VM to check if it is up, then
-#                	it tries to SSH into the VM until it gains access.
-#: Options      :	None required. None accepted.
-#: Usage		:	Call the script. No arguments needed.
-#					Example: ./RHCE_vm.sh
+#: Version      :	2.0 (Stable)
+#: Description  :	If given the argument 'on', this script first 
+#                   starts the VM tagged as "RHCE", then it pings the 
+#                   VM to check if it is up, then it tries to SSH into 
+#                   the VM until it gains access.
+#                   If given the argument 'off', this script turns off
+#                   the VM tagged as "RHCE", if it running.
+#: Options      :	One required. Either 'on' or 'off'
+#: Usage		:	Call the script. One argument needed.
+#					Example: ./RHCE_vm.sh on
+#                            ./RHCE_vm.sh off
 ################
 
 
-
-# Function to check if VM is running, and run it in case it is not.
-function start_vm() {
+# Function to control state of VM, depending on the argument given to the script
+# If the 'on' argument is given, this function starts the VM
+# If the 'off' argument is given, this function turns off the VM
+function control_vm() {
 	# UUID of 'RHCE' VM
 	local -r UUID="80fc2908-3ee6-4196-85d3-b7f484e883dc"
 
-	# If RHCE VM is running
-	if [[ $( VBoxManage list runningvms ) =~ ${UUID} ]]
+	# If the argument is "on"
+	if [[ "${FLAG}" == "on" ]]
 	then
-		printf '\n%s\n' "VM is running"
-	else
-		VBoxManage startvm --type headless "${UUID}" 1>/dev/null 2>&1 \
+		# If RHCE VM is running, print that it's on
+		if [[ $( VBoxManage list runningvms ) =~ ${UUID} ]]
+		then
+			printf '\n%s\n' "VM is running"
+		# If RHCE VM isn't running, turn it on
+		elif [[ ! $( VBoxManage list runningvms ) =~ ${UUID} ]]
+		then
+			VBoxManage startvm --type headless "${UUID}" 1>/dev/null 2>&1
 			printf '%s\n' "VM powered on"
+		fi
+	# If the argument is "off"
+	elif [[ "${FLAG}" == "off" ]]
+	then
+		# If RHCE VM is running, turn it off
+		if [[ $( VBoxManage list runningvms ) =~ ${UUID} ]]
+		then
+			printf '\n%s\n' "VM is running. Stopping VM..."
+			VBoxManage controlvm "${UUID}" poweroff
+		# If RHCE VM is not running, print that it's off
+		else
+			printf '\n%s\n' "VM is not running."
+		fi
 	fi
 }
 
@@ -39,6 +62,7 @@ function ping_vm() {
 	# for reference.
 	local -r IP="192.168.1.60"
 
+	printf '\n%s\n' "Trying to ping VM"
 	# Keep executing sleep command until ping command succeeds
 	until ping -c1 "${IP}" 1>/dev/null 2>&1 # Can also be written as &>/dev/null
 	do
@@ -52,8 +76,9 @@ function ssh_port() {
 	local -r IP="192.168.1.60"
 
 	# If VM is running and is responding to ICMP echo requests
-	if start_vm && ping_vm
+	if control_vm && ping_vm
 	then
+		printf '\n%s\n' "VM is responding to ICMP echo requests"
 		# Check if VM is accepting connections on port 22
 		until nc -z "${IP}" 22 1>/dev/null 2>&1
 		do
@@ -69,6 +94,7 @@ function ssh_login() {
 	if ssh_port
 	then
 		# Until the VM responds with an 'uptime' report, sleep for 5 seconds
+		printf '\n%s\n' "The SSH port for the VM is open and accepting connections"
 		until [[ $(ssh -q rhce uptime) =~ "load average" ]]
 		do
 			sleep 5
@@ -76,11 +102,48 @@ function ssh_login() {
 	fi
 }
 
-# If SSH connections can be made, start an SSH session
-if ssh_login
-then
-	ssh rhce
-fi
 
+# Function to check if argument provided to the script is as defined
+function check_input() {
+	if [[ $# -eq 1 ]]
+	then
+		case "${1}" in
+			"on")
+				FLAG="${1}"
+				return
+				;;
+			"off")
+				FLAG="${1}"
+				return
+				;;
+			*)
+				printf '\n%s\n\t%s\n\t%s\n' " $0 : Incorrect argument." "USAGE: $0 off" "USAGE: $0 on"
+				exit 1
+				;;
+		esac
+	else
+		printf '\n%s\n\t%s\n\t%s\n' " $0 : Incorrect argument." "USAGE: $0 off" "USAGE: $0 on"
+		exit 1
+	fi
+}
+
+# Check the input given to the script
+check_input "$@"
+
+# The argument given is 'on'
+if [[ "${FLAG}" == "on" ]]
+then
+	# If SSH connections can be made, start an SSH session
+	if ssh_login
+	then
+		printf '\n%s\n' "VM can be accessed via SSH. Logging in..."
+		ssh -q rhce
+	fi
+# If the argument given is 'off'
+elif [[ "${FLAG}" == "off" ]]
+then
+	# Turn off the VM
+	control_vm
+fi
 
 # End of script
