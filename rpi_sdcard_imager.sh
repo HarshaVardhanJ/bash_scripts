286ec2214244ae8567e4090e1139f43f94c97c19
Harsha Vardhan Jagadeesan <vardhanharshaj@gmail.com>
2018-12-28 20:52:34 +0530

    Removed single quotes around SSID and password in WiFi setup.

--- rpi_sdcard_imager.sh
+++ rpi_sdcard_imager.sh
@@ -779,9 +779,9 @@ ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 
 network={
-	ssid='"${SSID}"'
+	ssid="${SSID}"
 	key-mgmt=WPA2-PSK
-	psk='"$( cat "${TEMP}" )"'
+	psk="$( cat "${TEMP}" )"
 }
 EOF
 
